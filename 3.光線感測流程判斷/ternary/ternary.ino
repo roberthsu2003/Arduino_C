f4147fde91f23e72d9ea911b5d51314bcecd8b73
roberthsu2003 <roberthsu2003@gmail.com>
2020-05-01 19:10:37 +0800

    modify

--- 3.光線感測流程判斷/ternary/ternary.ino
+++ 3.光線感測流程判斷/ternary/ternary.ino
@@ -1,17 +1,17 @@
 //arduinoC
 /*
-請使用三元運算子? : 製作一個判斷奇偶數的程式，程式的執行結果
+請使用三元運算子? : 製作一個判斷奇偶數的程式，程式的執行結果,偶數蜂鳴器叫2次,奇數蜂鳴器叫1次
 顯示=============
 請輸入數字:15
 您輸入的15是奇數
  */
-
-#include <MatrixMini.h>
-MatrixMini Mini;
-
+ 
+#define buzzer 3
 
 void setup(){
-  Mini.begin();
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, HIGH);
+  
   Serial.begin(9600);  
   Serial.println("請輸入數字:");  
 }
@@ -20,12 +20,31 @@ void loop() {
   int inputNumber;
   String message;
   if(Serial.available()){
-    Mini.LED1.setRGB(0,0,0);
-    Mini.LED2.setRGB(0,0,0);
     inputNumber = Serial.parseInt();
     
     message = (inputNumber % 2 == 0) ? "偶數" : "奇數";
-       
+
+    switch(inputNumber % 2){
+      case 0:
+        
+        digitalWrite(buzzer, LOW);
+        delay(200);
+        digitalWrite(buzzer, HIGH);
+        delay(200);
+        digitalWrite(buzzer, LOW);
+        delay(200);
+        digitalWrite(buzzer, HIGH);
+        delay(200);
+        break;
+      case 1:
+        
+        digitalWrite(buzzer, LOW);
+        delay(200);
+        digitalWrite(buzzer, HIGH);
+        delay(200);
+        break;
+      }
+      
     Serial.print("您輸入的");
     Serial.print(inputNumber);
     Serial.print("是");
