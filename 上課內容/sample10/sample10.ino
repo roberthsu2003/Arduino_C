32536bf27dfa5abe49846aaf8ec38dc25ae2ffe7
Robert Hsu <roberthsu2003@gmail.com>
2020-07-05 16:35:29 +0800

    Update sample10.ino

--- 上課內容/sample10/sample10.ino
+++ 上課內容/sample10/sample10.ino
@@ -1,18 +1,23 @@
 #include <MatrixMini.h>
+#define buzzer 3
 
 MatrixMini Mini;
 
 void setup() {
   Mini.begin();
   Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+  digitalWrite(buzzer, HIGH);
 
 }
 
 void loop() {
   if(Mini.BTN1.get() && Mini.BTN2.get()){
     Serial.println("同時按下");
+    digitalWrite(buzzer, LOW);
   }else{
     Serial.println("沒有同時按下");
+    digitalWrite(buzzer, HIGH);
   }
 
 }
