6e61aec17d51f6abb9433d5d0197cb14628d492a
Robert Hsu <roberthsu2003@gmail.com>
2020-07-05 16:47:17 +0800

    Update sample10.ino

--- 上課內容/sample10/sample10.ino
+++ 上課內容/sample10/sample10.ino
@@ -12,11 +12,23 @@ void setup() {
 }
 
 void loop() {
+  /*
   if(Mini.BTN1.get() && Mini.BTN2.get()){
     Serial.println("同時按下");
     digitalWrite(buzzer, LOW);
   }else{
     Serial.println("沒有同時按下");
+    digitalWrite(buzzer, HIGH);
+  }
+  */
+  //使用巢狀判斷
+  if(Mini.BTN1.get()){
+    if(Mini.BTN2.get()){
+      digitalWrite(buzzer, LOW);
+    }else{
+      digitalWrite(buzzer, HIGH);
+    }
+  }else{
     digitalWrite(buzzer, HIGH);
   }
 
