be03b77aebacc62ca6808f84ed3116c82b028f32
Robert Hsu <roberthsu2003@gmail.com>
2020-07-05 12:31:00 +0800

    Create sample8.ino

---
+++ 上課內容/sample8/sample8.ino
@@ -0,0 +1,18 @@
+#include <MatrixMini.h>
+#define LIGHT_SENSOR A1
+#define LIGHT_DIGITOR A0
+
+MatrixMini Mini;
+void setup() {
+  Mini.begin();
+  Serial.begin(9600);
+  pinMode(LIGHT_DIGITOR, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Digitor:");
+  Serial.println(digitalRead(LIGHT_DIGITOR));
+  delay(500);
+}
