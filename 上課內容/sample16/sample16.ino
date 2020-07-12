f7bd08ca6c3a23329e77bacbc104c0fab76f7f58
Robert Hsu <roberthsu2003@gmail.com>
2020-07-12 14:37:43 +0800

    Create sample16.ino

---
+++ 上課內容/sample16/sample16.ino
@@ -0,0 +1,22 @@
+long randomNumber;
+
+void setup() {
+// put your setup code here, to run once:
+Serial.begin(9600);
+randomSeed(analogRead(0));
+}
+
+void loop() {
+
+  // 列印出0~299的亂數
+  
+  randomNumber = random(300);
+  Serial.println(randomNumber);
+  delay(1000);
+
+  // 列印出10~19的亂數
+  randomNumber = random(10, 20);
+  Serial.println(randomNumber);
+  delay(1000);
+ 
+}
