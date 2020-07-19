1953e3a2ead8e76d1a07c6f31687d8df4f16799e
Robert Hsu <roberthsu2003@gmail.com>
2020-07-19 10:36:50 +0800

    Update sample19.ino

--- 上課內容/sample19/sample19.ino
+++ 上課內容/sample19/sample19.ino
@@ -9,21 +9,48 @@
 MatrixMini mini;
 
 Thread myThread = Thread();
+Thread myThread3 = Thread();
+Thread myThread5 = Thread();
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   mini.begin();
   myThread.onRun(callBack1);
   myThread.setInterval(1000);
+  myThread3.onRun(callBack2);
+  myThread3.setInterval(3000);
+  myThread5.onRun(callBack3);
+  myThread5.setInterval(5000);
 }
 
-void callBack1(){
+void callBack1() {
   Serial.println("1秒Run");
 }
 
+
+void callBack2() {
+  Serial.println("3秒Run");
+}
+
+void callBack3() {
+  Serial.println("5秒Run");
+}
+
 void loop() {
-  if(myThread.shouldRun()){
+  threadRun();
+
+}
+
+void threadRun() {
+  if (myThread.shouldRun()) {
     myThread.run();
   }
 
+  if (myThread3.shouldRun()) {
+    myThread3.run();
+  }
+
+  if (myThread5.shouldRun()) {
+    myThread5.run();
+  }
 }
