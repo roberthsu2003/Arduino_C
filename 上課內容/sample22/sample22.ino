a5b784e64abc7426c6cef12692777115d1015018
Robert Hsu <roberthsu2003@gmail.com>
2020-07-19 15:45:18 +0800

    Create sample22.ino

---
+++ 上課內容/sample22/sample22.ino
@@ -0,0 +1,26 @@
+//紅外線校對
+//循跡車
+//人面對背面方向
+//D1插孔 - 紅外線左模組
+//D2插孔- 紅外線右模組
+
+//白色-0
+//黑色-1
+#define irLeft 2
+#define irRight 3
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int leftValue = digitalRead(irLeft);
+  int rightValue = digitalRead(irRight);
+  Serial.print("left:");
+  Serial.println(leftValue);
+  Serial.print("right:");
+  Serial.println(rightValue);
+  delay(500);
+}
