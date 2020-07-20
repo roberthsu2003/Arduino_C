cf83d5a973a06ccf2397edbb6645eb2a76b4cd3c
Robert Hsu <roberthsu2003@gmail.com>
2020-07-20 13:31:42 +0800

    Update sample1.ino

--- 早上青少年上課內容/sample1/sample1.ino
+++ 早上青少年上課內容/sample1/sample1.ino
@@ -1,10 +1,23 @@
+//藍線A0 - Echo
+//白線A1 - Trig
+//安裝NewPing Library
+
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  A1
+#define ECHO_PIN     A0
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello!Arduino");
-
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(50);
+  int uS = sonar.ping();
+  Serial.print("Ping: ");
+  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.println("cm");
 }
