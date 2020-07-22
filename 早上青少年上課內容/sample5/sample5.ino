a73727e8213a40ea620ca4831c56f5668f91affd
Robert Hsu <roberthsu2003@gmail.com>
2020-07-22 12:03:14 +0800

    Update sample5.ino

--- 早上青少年上課內容/sample5/sample5.ino
+++ 早上青少年上課內容/sample5/sample5.ino
@@ -1,9 +1,18 @@
 int area;
-int radius = 35;
+int radius;
 //float PI = 3.14159;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("請輸入半徑:");
+  //等待使用者輸入半徑
+  while(true){
+    if(Serial.available()){
+      radius = Serial.parseInt();
+      break;
+    }
+  }
+  
   area = PI * radius * radius;
   Serial.print("圓面積=");
   Serial.println(area);
