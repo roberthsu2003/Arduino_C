6b19a2acccc6a1882fd8385ae819268e62225590
Robert Hsu <roberthsu2003@gmail.com>
2020-07-22 10:38:16 +0800

    Update sample2.ino

--- 早上青少年上課內容/sample2/sample2.ino
+++ 早上青少年上課內容/sample2/sample2.ino
@@ -1,9 +1,10 @@
 void setup() {
   Serial.begin(9600);
-  Serial.println("您好,Arduino");
+  Serial.println("您好,這裏是setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("您好,這裏是loop");
+  delay(1000);
 }
