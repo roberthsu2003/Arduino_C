3c146fe9f4368234bd7a85f0a0c4eb38ceeab283
Robert Hsu <roberthsu2003@gmail.com>
2020-07-24 11:35:12 +0800

    Update sample9.ino

--- 早上青少年上課內容/sample9/sample9.ino
+++ 早上青少年上課內容/sample9/sample9.ino
@@ -7,9 +7,27 @@ void setup() {
 
 void loop() {
   int money;
+  int payMoney;
   if(Serial.available()){
     money = Serial.parseInt();
     Serial.print("您購買的金額是:");
     Serial.println(money);
+
+    if(money >= 10000){
+      payMoney = money * 0.8;
+    }else if(money >= 5000){
+      payMoney = money * 0.85;
+    }else if(money >= 3000){
+      payMoney = money * 0.9;
+    }else if(money >= 1000){
+      payMoney = money * 0.95;
+    }else{
+      payMoney = money;
+    }
+
+    Serial.print("實付金額是:");
+    Serial.print(payMoney);
+    Serial.println("元");
+    Serial.print("==================");
   }
 }
