fb4fc190b7d607c15c31023d14f67ce858a330d6
Robert Hsu <roberthsu2003@gmail.com>
2020-07-26 10:38:04 +0800

    Update sample24.ino

--- 上課內容/sample24/sample24.ino
+++ 上課內容/sample24/sample24.ino
@@ -1,23 +1,39 @@
 //藍線A0 - Echo
 //白線A1 - Trig
 //安裝NewPing Library
+//人面對背面方向
+//D1插孔 - 紅外線左模組
+//D2插孔- 紅外線右模組
+
 
 #include <NewPing.h>
 
 #define TRIGGER_PIN A1
 #define ECHO_PIN A0
 #define MAX_DISTANCE 200
+#define irLeft 2
+#define irRight 3
 
 NewPing sonar(TRIGGER_PIN,ECHO_PIN,MAX_DISTANCE);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  pinMode(irLeft,INPUT);
+  pinMode(irRight,INPUT);
 }
 
 void loop() {
-  delay(50);
+  
   int uS= sonar.ping();
   Serial.print("Ping: ");
   Serial.print(uS / US_ROUNDTRIP_CM);
   Serial.println("cm");
+
+  int leftValue = digitalRead(irLeft);
+  int rightValue = digitalRead(irRight);
+  Serial.print("left:");
+  Serial.println(leftValue);
+  Serial.print("right:");
+  Serial.println(rightValue);
+  delay(500);
 }
