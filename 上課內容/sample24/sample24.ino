109f4d1416624e877cd8ed6fc8c0c838f163b7bc
Robert Hsu <roberthsu2003@gmail.com>
2020-07-26 15:03:44 +0800

    Update sample24.ino

--- 上課內容/sample24/sample24.ino
+++ 上課內容/sample24/sample24.ino
@@ -1,3 +1,4 @@
+//小車循跡S區線，終點使用障礙物，讓小車轉一圈後循跡S區線，回到起點
 //藍線A0 - Echo
 //白線A1 - Trig
 //安裝NewPing Library
@@ -8,6 +9,7 @@
 
 #include <NewPing.h>
 #include <MatrixMini.h>
+#include <Thread.h>
 
 #define TRIGGER_PIN A1
 #define ECHO_PIN A0
@@ -16,6 +18,7 @@
 #define irRight 3
 
 NewPing sonar(TRIGGER_PIN,ECHO_PIN,MAX_DISTANCE);
+Thread myThread = Thread();
 MatrixMini Mini;
 
 void setup() {
@@ -23,10 +26,26 @@ void setup() {
   Mini.begin(); 
   pinMode(irLeft,INPUT);
   pinMode(irRight,INPUT);
-  oneTurn();//測試轉一圈
+  //oneTurn();//測試轉一圈
+  myThread.onRun(runS);
+  myThread.setInterval(50);
 }
 
-void loop() {  
+void loop() {
+  if (goOrStop()){
+    if(myThread.shouldRun()){
+      myThread.run(); 
+    }
+  }else{
+    //停車
+    Mini.M1.set(0);
+    Mini.M2.set(0);
+    delay(2000);
+    oneTurn();
+  }
+  
+  
+  /*
   int uS= sonar.ping();
   Serial.print("Ping: ");
   Serial.print(uS / US_ROUNDTRIP_CM);
@@ -39,15 +58,55 @@ void loop() {
   Serial.print("right:");
   Serial.println(rightValue);
   delay(500);
+  */
+}
+
+bool goOrStop(){
+  int uS= sonar.ping();
+  int distance = uS / US_ROUNDTRIP_CM;
+  /*
+  Serial.print("Ping: ");
+  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.println("cm");
+  */
+  return (distance > 10) ?  true : false;  
+}
+
+void runS(){
+  Serial.println("runS");
+  int leftValue = digitalRead(irLeft);
+  int rightValue = digitalRead(irRight);
+  int speed = 50;
+  int g = 20;
+  if(leftValue == 0 && rightValue == 0){
+    //直線
+    running(speed, speed);
+    Serial.println("直線");
+  }else if(leftValue == 0 && rightValue == 1){
+    //右轉
+    running(speed, speed-g);
+    Serial.println("右轉");
+  }else if(leftValue == 1 && rightValue == 0){
+    //左轉
+    running(speed-g, speed);
+    Serial.println("左轉");
+  }
+}
+
+void running(int leftMotor, int rightMotor){
+  //M1 = 左輪
+  //M2 = 右輪
+  Mini.M1.set(leftMotor);
+  Mini.M2.set(rightMotor);
 }
 
 void oneTurn(){
-  int interval = 6500;
-  int speed = 20;
+  int interval = 3700;
+  int speed = 30;
   Mini.M1.set(speed);
   Mini.M2.set(-speed);
   delay(interval);
   Mini.M1.set(0);
   Mini.M2.set(0);
-  while(true);
+  //while(true);
 }
