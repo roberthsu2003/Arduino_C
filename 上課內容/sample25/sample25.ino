a34b76691dad022d6a2bbe88e64fd23422f3184e
Robert Hsu <roberthsu2003@gmail.com>
2020-07-26 11:18:44 +0800

    Create sample25.ino

---
+++ 上課內容/sample25/sample25.ino
@@ -0,0 +1,14 @@
+//建立陣列語法
+
+void setup() {
+  Serial.begin(9600);
+  int scores[3] = {70, 80, 90};
+  Serial.println(scores[0]);
+  Serial.println(scores[1]);
+  Serial.println(scores[2]);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
