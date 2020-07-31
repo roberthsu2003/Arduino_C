da3a51aa99334a0820cb37f7aa8277b383bb72d0
Robert Hsu <roberthsu2003@gmail.com>
2020-07-31 10:51:12 +0800

    Update sample17.ino

--- 早上青少年上課內容/sample17/sample17.ino
+++ 早上青少年上課內容/sample17/sample17.ino
@@ -9,11 +9,29 @@ void setup() {
   pinMode(BUZZER, OUTPUT);
   int min = 1;
   int max = 99;
-  int guess;
+  int guess; // 隨機的數值
+  int keyin; //使用者輸入的值
+  int count = 0;
   //建立亂數
   randomSeed(analogRead(0));
   guess = random(min, max+1);
-  Serial.println("現在的亂數是" + (String)guess);
+  //Serial.println("現在的亂數是" + (String)guess);
+  Serial.println("===============猜數字遊戲=================:\n");
+  do{
+    //顯示猜數字的範圍
+    Serial.print("猜數字範圍" + (String)min + "~" + (String)max + ":");
+
+    //等待和接收使用者輸入,並且顯示輸入的數值
+    while(true){
+      if(Serial.available()){
+        keyin = Serial.parseInt();
+        count++;
+        s.beep(200);
+        Serial.println(keyin);
+        break;
+      }
+    }
+  }while(true);
   
 }
 
