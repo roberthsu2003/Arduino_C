216eb4d708dffb2670b6789666ecf428789ba255
Robert Hsu <roberthsu2003@gmail.com>
2020-08-03 10:54:44 +0800

    Update sample21.ino

--- 早上青少年上課內容/sample21/sample21.ino
+++ 早上青少年上課內容/sample21/sample21.ino
@@ -5,13 +5,13 @@ void setup() {
   Mini.begin();
   Mini.M1.set(50);
   Mini.M2.set(50);
-  delay(2000);
+  delay(3000);
   Mini.M1.set(50);
-  Mini.M2.set(20);
+  Mini.M2.set(-50);
   delay(1000);
   Mini.M1.set(50);
   Mini.M2.set(50);
-  delay(2000);
+  delay(3000);
   Mini.M1.set(0);
   Mini.M2.set(0);
 }
