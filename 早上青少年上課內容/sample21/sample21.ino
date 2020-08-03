8157fcdd3370524ac527a5892d2a1486107fd8e5
Robert Hsu <roberthsu2003@gmail.com>
2020-08-03 10:15:53 +0800

    Create sample21.ino

---
+++ 早上青少年上課內容/sample21/sample21.ino
@@ -0,0 +1,16 @@
+#include <MatrixMini.h>
+MatrixMini Mini;
+
+void setup() {
+  Mini.begin();
+  Mini.M1.set(50);
+  Mini.M2.set(-50);
+  delay(3000);
+  Mini.M1.set(0);
+  Mini.M2.set(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
