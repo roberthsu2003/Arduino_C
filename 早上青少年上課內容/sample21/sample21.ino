6553cd87a168b22cea17e2b172b0eda6eee1b5d1
Robert Hsu <roberthsu2003@gmail.com>
2020-08-03 10:28:55 +0800

    Update sample21.ino

--- 早上青少年上課內容/sample21/sample21.ino
+++ 早上青少年上課內容/sample21/sample21.ino
@@ -4,8 +4,14 @@ MatrixMini Mini;
 void setup() {
   Mini.begin();
   Mini.M1.set(50);
-  Mini.M2.set(-50);
-  delay(3000);
+  Mini.M2.set(50);
+  delay(2000);
+  Mini.M1.set(50);
+  Mini.M2.set(0);
+  delay(1000);
+  Mini.M1.set(50);
+  Mini.M2.set(50);
+  delay(2000);
   Mini.M1.set(0);
   Mini.M2.set(0);
 }
