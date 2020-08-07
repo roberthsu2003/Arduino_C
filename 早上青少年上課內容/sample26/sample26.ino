76700f771af3549e9420bbb48bd8e973c13061e8
Robert Hsu <roberthsu2003@gmail.com>
2020-08-07 09:45:04 +0800

    modify

---
+++ 早上青少年上課內容/sample26/sample26.ino
@@ -0,0 +1,24 @@
+#include <MatrixMini.h>
+
+MatrixMini Mini;
+
+void setup() {
+  Mini.begin();
+  oneTurn();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void oneTurn(){
+  //轉360
+  int interval = 3000;
+  int speed = 50;
+  Mini.M1.set(speed);
+  Mini.M2.set(-speed);
+  delay(interval);
+  Mini.M1.set(0);
+  Mini.M2.set(0);
+}
