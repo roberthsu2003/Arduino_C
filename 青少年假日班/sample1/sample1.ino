f351976e05ad6f044e6a8643c6f42c9821c54cad
Robert Hsu <roberthsu2003@gmail.com>
2020-08-22 14:20:29 +0800

    Update sample1.ino

--- 青少年假日班/sample1/sample1.ino
+++ 青少年假日班/sample1/sample1.ino
@@ -1,25 +1,31 @@
+//使用光敏電阻,檢查目前的亮度
+//插入至Ａ1孔
+//pin A1-白色線(類比訊號)
+//pin A0-藍色線(數位訊號)
+
+
 #include <MatrixMini.h>
+#define LIGHT_SENSOR A1
+#define LIGHT_DIGHTOR A0
 
 MatrixMini Mini;
 
 void setup() {
-  Mini.begin(); 
+  Mini.begin();
   Serial.begin(9600);
-
-  Serial.println("\n Matrix Mini Button LED Test \n");
+  Serial.println("==========光敏電阻測試==============");
+  pinMode(LIGHT_DIGHTOR, INPUT);
 }
 
 void loop() {
-  if(Mini.BTN1.get()) 
-    Mini.LED1.setRGB(255,0, 0);
-  else
-    Mini.LED1.setRGB(0,0, 0);
-
-
-  if(Mini.BTN2.get()) 
-    Mini.LED2.setRGB(0,255, 0);
-  else
-    Mini.LED2.setRGB(0,0, 0);
-  
-  delay(50);
+  Serial.println("==========光敏電阻數位訊號輸出==============");
+  bool digitalValue = digitalRead(LIGHT_DIGHTOR);
+  Serial.print("數位訊號是:");
+  Serial.println(digitalValue);
+  Serial.println("\n\n");
+  Serial.println("==========光敏電阻類比訊號輸出==============");
+  int analogValue = analogRead(LIGHT_SENSOR);
+  Serial.print("類比訊號是:");
+  Serial.println(analogValue);
+  delay(1000);
 }
