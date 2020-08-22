b650bce5e231611786d690fecd17f7e791cd0d28
Robert Hsu <roberthsu2003@gmail.com>
2020-08-22 15:10:22 +0800

    Update sample1.ino

--- 青少年假日班/sample1/sample1.ino
+++ 青少年假日班/sample1/sample1.ino
@@ -1,26 +1,35 @@
-//人面對背面方向
-//D1插孔 - 紅外線左模組
-//D2插孔- 紅外線右模組
+#include <MatrixMini.h>
 
-//白色-0
-//黑色-1
-#define irLeft 2
-#define irRight 3
+MatrixMini Mini;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(irLeft,INPUT);
-  pinMode(irRight,INPUT);
+  Mini.begin();  // create with the default frequency 1.6KHz
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  
+  Serial.println("\n Matrix Mini DC Motor Test \n");
+
+  Mini.M1.set(0);
+  Mini.M2.set(0);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  int leftValue = digitalRead(irLeft);
-  int rightValue = digitalRead(irRight);
-  Serial.print("left:");
-  Serial.println(leftValue);
-  Serial.print("right:");
-  Serial.println(rightValue);
-  delay(500);
+  int i;
+  
+  Serial.println("One way");
+  for (i=-100; i<100; i++) {
+    Mini.M1.set(i);  
+    Mini.M2.set(i);
+    delay(15);
+  }
+  
+  Serial.println("Or another");
+  for (i=100; i>-100; i--) {
+    Mini.M1.set(i);  
+    Mini.M2.set(i);
+    delay(15);
+  }
+  
 }
