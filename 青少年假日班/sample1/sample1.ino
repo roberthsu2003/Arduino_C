4d568d256b4014b52e612246d113b7d967ce48e8
Robert Hsu <roberthsu2003@gmail.com>
2020-08-22 14:53:12 +0800

    Update sample1.ino

--- 青少年假日班/sample1/sample1.ino
+++ 青少年假日班/sample1/sample1.ino
@@ -1,24 +1,26 @@
-//安裝在A1孔
-//藍線A0 - Trig
-//白線A1 - Echo
-//安裝NewPing Library
+//人面對背面方向
+//D1插孔 - 紅外線左模組
+//D2插孔- 紅外線右模組
 
-#include <NewPing.h>
- 
-#define TRIGGER_PIN  A0
-#define ECHO_PIN     A1
-#define MAX_DISTANCE 200
- 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
- 
+//白色-0
+//黑色-1
+#define irLeft 2
+#define irRight 3
+
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(irLeft,INPUT);
+  pinMode(irRight,INPUT);
 }
- 
+
 void loop() {
-  delay(50);
-  int uS = sonar.ping();
-  Serial.print("Ping: ");
-  Serial.print(uS / US_ROUNDTRIP_CM);
-  Serial.println("cm");
+  // put your main code here, to run repeatedly:
+  int leftValue = digitalRead(irLeft);
+  int rightValue = digitalRead(irRight);
+  Serial.print("left:");
+  Serial.println(leftValue);
+  Serial.print("right:");
+  Serial.println(rightValue);
+  delay(500);
 }
