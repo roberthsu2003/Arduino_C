a31b34ed48da8259c6e4cd8ebf8f2f0e33c202fc
Robert Hsu <roberthsu2003@gmail.com>
2020-08-22 16:20:34 +0800

    Create sample2.ino

---
+++ 青少年假日班/sample2/sample2.ino
@@ -0,0 +1,24 @@
+//arduinoC
+//arduinoC的輸入輸出
+
+void setup() {
+  Serial.begin(9600);
+  int englishScore = 60;
+  Serial.println("請輸入您的英文分數:");
+  //等待使用者輸入
+  while(true){
+    if(Serial.available()){
+      englishScore = Serial.parseInt();
+      break;
+    }
+  }
+
+  Serial.print("Hello! 您的英文分數是:");
+  Serial.println(englishScore);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
