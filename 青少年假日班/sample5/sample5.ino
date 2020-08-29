c1bfa7a09b85ab341af641f60e31fc8f2af8bccc
Robert Hsu <roberthsu2003@gmail.com>
2020-08-29 10:55:43 +0800

    Create sample5.ino

---
+++ 青少年假日班/sample5/sample5.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+  bool bolA = true;
+  bool bolB = false;
+  Serial.println("bolA=" + (String)bolA);
+  Serial.println("bolB=" + (String)bolB);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
