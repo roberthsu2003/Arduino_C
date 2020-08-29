3cee54cfc0b596c4c77548f7658ecaabb6f08608
Robert Hsu <roberthsu2003@gmail.com>
2020-08-29 10:14:57 +0800

    Create sample3.ino

---
+++ 青少年假日班/sample3/sample3.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.print("姓名\t座號\t電話\n");
+  Serial.print("李振聲\t11\t0220435437\n");
+  Serial.print("張四聲\t12\t0220435437\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
