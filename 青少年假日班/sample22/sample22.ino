a95ccf862c66974b416c355f2f22e3d94d96e4a9
Robert Hsu <roberthsu2003@gmail.com>
2020-09-05 16:53:45 +0800

    Create sample22.ino

---
+++ 青少年假日班/sample22/sample22.ino
@@ -0,0 +1,49 @@
+#include <MatrixMini.h>
+#include <Thread.h>
+
+MatrixMini Mini;
+Thread timeThread = Thread();
+
+void setup() {
+  Serial.begin(9600);
+  Mini.begin();
+  randomSeed(analogRead(A0) + millis()/2);
+
+  timeThread.onRun(callBack);
+  timeThread.setInterval(500);
+}
+
+void callBack(){
+  static bool ledStatus = false;
+  ledStatus = !ledStatus;
+  int randomValue = random(1000);
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  switch(randomValue%3){
+    case 0:
+      r = 255;
+      break;
+    case 1:
+      g = 255;
+      break;
+    case 2:
+      b = 255;
+      break; 
+  }
+
+  if(ledStatus == LOW){
+    Mini.LED1.setRGB(r, g, b);
+    Mini.LED2.setRGB(r, g, b);
+  }else{
+    Mini.LED1.setRGB(r, g, b);
+    Mini.LED2.setRGB(r, g, b);
+  }
+}
+
+void loop() {
+  if(timeThread.shouldRun()){
+    timeThread.run();
+  }
+
+}
