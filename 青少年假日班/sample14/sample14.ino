22d9fe7a0a26bb78797e367b6b1b794470c42ea4
Robert Hsu <roberthsu2003@gmail.com>
2020-09-05 10:26:48 +0800

    Update sample14.ino

--- 青少年假日班/sample14/sample14.ino
+++ 青少年假日班/sample14/sample14.ino
@@ -4,6 +4,16 @@ void setup() {
   Serial.begin(9600);
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, HIGH);
+
+  int sum = 0;
+  for(int i=2; i<=10; i+=2){
+    sum += i;
+    Serial.println("第"+String(i/2) +"次迴圈的i=" + String(i) + "總和為" + String(sum));
+    digitalWrite(BUZZER, LOW);
+    delay(100);
+    digitalWrite(BUZZER, HIGH);
+    delay(100);
+  }
 }
 
 void loop() {
