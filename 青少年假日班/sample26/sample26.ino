6134ed729d42906f5d2eca4c981cf49ba1e242f1
Robert Hsu <roberthsu2003@gmail.com>
2020-09-12 11:10:15 +0800

    Create sample26.ino

---
+++ 青少年假日班/sample26/sample26.ino
@@ -0,0 +1,19 @@
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  A0
+#define ECHO_PIN     A1
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  delay(50);
+  int uS = sonar.ping();
+  Serial.print("Ping: ");
+  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.println("cm");
+}
