c0fe72d57c3b17e77ff2b2d9a27b2db8ab8828be
Robert Hsu <roberthsu2003@gmail.com>
2020-09-12 11:44:05 +0800

    Update sample26.ino

--- 青少年假日班/sample26/sample26.ino
+++ 青少年假日班/sample26/sample26.ino
@@ -1,19 +1,48 @@
 #include <NewPing.h>
+#include <Thread.h>
  
 #define TRIGGER_PIN  A0
 #define ECHO_PIN     A1
 #define MAX_DISTANCE 200
  
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+Thread myThread = Thread();
+
+byte distances[10];
  
 void setup() {
   Serial.begin(9600);
+  myThread.onRun(callBack);
+  myThread.setInterval(100);
 }
+
+void callBack(){
+  static byte count = 0;
+  static int sumDis = 0;
+  if(count == 10){
+    for(int i=0; i<=9; i++){
+      sumDis += distances[i];
+    }
+
+    Serial.print(sumDis/10);
+    Serial.println("cm");
+    count = 0;
+    sumDis = 0;
+  }
+
+  int uS = sonar.ping();
+  if(uS != 0){
+    distances[count] = uS / US_ROUNDTRIP_CM;
+    count ++;
+  }
+}
+
+  
+  
+ 
  
 void loop() {
-  delay(50);
-  int uS = sonar.ping();
-  Serial.print("Ping: ");
-  Serial.print(uS / US_ROUNDTRIP_CM);
-  Serial.println("cm");
+  if(myThread.shouldRun()){
+    myThread.run();
+  }
 }
