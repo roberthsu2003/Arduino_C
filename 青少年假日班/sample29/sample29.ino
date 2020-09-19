61cf262f3e4ae8ce460d855235528d5ba24e2db1
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 10:17:30 +0800

    Update sample29.ino

--- 青少年假日班/sample29/sample29.ino
+++ 青少年假日班/sample29/sample29.ino
@@ -1,9 +1,6 @@
 void setup() {
   Serial.begin(9600);
-  int scores[3];
-  scores[0] = 70;
-  scores[1] = 80;
-  scores[2] = 90;
+  int scores[3] = {70, 80, 90}; //同時給值的動作,只有在建立陣列時可以使用,不可以後面才一次給值
 
   Serial.println("scores[0]=" + String(scores[0]));
   Serial.println("scores[1]=" + String(scores[1]));
