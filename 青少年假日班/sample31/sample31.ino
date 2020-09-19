34231fed7bedbd6aaa7062ef31699138109533b5
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 11:03:52 +0800

    Update sample31.ino

--- 青少年假日班/sample31/sample31.ino
+++ 青少年假日班/sample31/sample31.ino
@@ -7,8 +7,9 @@ void setup() {
   Serial.begin(9600);
   delay(2000);
   for(int i=0; i<10; i++){
-    Mini.M1.set(50);
+    Mini.M1.set(80);
     Mini.M2.set(0);
+    delay(100);
   }
   Mini.M1.set(0);
   Mini.M2.set(0);
