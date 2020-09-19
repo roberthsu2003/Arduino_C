b17a0551eca2b0b6c3960c9874ec9080f2debb28
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 10:10:25 +0800

    Update sample29.ino

--- 青少年假日班/sample29/sample29.ino
+++ 青少年假日班/sample29/sample29.ino
@@ -5,9 +5,9 @@ void setup() {
   scores[1] = 80;
   scores[2] = 90;
 
-  Serial.println("scores[0]=" + scores[0]);
-  Serial.println("scores[1]=" + scores[1]);
-  Serial.println("scores[2]=" + scores[2]);
+  Serial.println("scores[0]=" + String(scores[0]));
+  Serial.println("scores[1]=" + String(scores[1]));
+  Serial.println("scores[2]=" + String(scores[2]));
 }
 
 void loop() {
