c8f366d80a467181121116f4c2748a985c66f050
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 10:53:49 +0800

    Create sample31.ino

---
+++ 青少年假日班/sample31/sample31.ino
@@ -0,0 +1,21 @@
+#include <MatrixMini.h>
+
+MatrixMini Mini;
+
+void setup() {
+  Mini.begin();
+  Serial.begin(9600);
+  delay(2000);
+  for(int i=0; i<10; i++){
+    Mini.M1.set(50);
+    Mini.M2.set(0);
+  }
+  Mini.M1.set(0);
+  Mini.M2.set(0);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
