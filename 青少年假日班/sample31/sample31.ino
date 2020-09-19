3c764ea20d974369e7950bbda71182fe27354c61
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 11:53:52 +0800

    Update sample31.ino

--- 青少年假日班/sample31/sample31.ino
+++ 青少年假日班/sample31/sample31.ino
@@ -1,18 +1,32 @@
 #include <MatrixMini.h>
+#include <NewPing.h>
+#define TRIGGER_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 200
 
 MatrixMini Mini;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+const int count=20;
+int distances[20];
 
 void setup() {
   Mini.begin();
   Serial.begin(9600);
   delay(2000);
-  for(int i=0; i<20; i++){
+  for(int i=0; i<count; i++){
     Mini.M1.set(50);
     Mini.M2.set(-50);
+    int uS = sonar.ping();
+    distances[i] = uS / US_ROUNDTRIP_CM;
     delay(100);
   }
   Mini.M1.set(0);
   Mini.M2.set(0);
+
+  for(int i=0; i<count; i++){
+    Serial.println(distances[i]);
+  }
 
 }
 
