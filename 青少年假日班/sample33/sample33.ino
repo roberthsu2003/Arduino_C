e013019ffe2b5f2809b0472da9274fe7f6b77e4b
Robert Hsu <roberthsu2003@gmail.com>
2020-09-19 14:24:38 +0800

    Update sample33.ino

--- 青少年假日班/sample33/sample33.ino
+++ 青少年假日班/sample33/sample33.ino
@@ -1,6 +1,8 @@
 //小車循跡S區線，終點使用障礙物，讓小車轉一圈後循跡S區線，回到起點
 //D1插孔 - 紅外線左模組
 //D2插孔- 紅外線右模組
+//白色-0
+//黑色-1
 
 #include <Thread.h>
 
@@ -14,7 +16,7 @@ void setup() {
   pinMode(irLeft,INPUT);
   pinMode(irRight,INPUT);
   myThread.onRun(runS);
-  myThread.setInterval(50);
+  myThread.setInterval(500);
 }
 
 void loop() {
@@ -26,4 +28,9 @@ void loop() {
 
 void runS(){
   Serial.println("0.05Run");
+  int leftValue = digitalRead(irLeft);
+  int rightValue = digitalRead(irRight);
+  Serial.println("leftValue=" + String(leftValue));
+  Serial.println("rightValue=" + String(rightValue));
+  
 }
