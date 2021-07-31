5d523cbb10c640f1a704a89bf036a5ba1f365574
Robert Hsu <roberthsu2003@gmail.com>
2021-07-31 20:13:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,13 @@
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+  randomSeed(analogRead(A1));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(A1));
-  delay(50);
+  Serial.println(random(1,101));
+  
+  delay(1000);
 }
