25359f3ba2d35ba9e46f45f1008bb0c704ef3092
Robert Hsu <roberthsu2003@gmail.com>
2021-07-31 20:26:14 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,23 @@
+//每隔2秒，檢查如果是偶數，亮綠燈，如果是奇數就亮紅燈
+#include <MatrixMini.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Mini.begin();
   Serial.begin(9600);
   randomSeed(analogRead(A1));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(random(1,101));
+  int randomNum = random(1001);
+  if(randomNum % 2 == 0){
+    //代表偶數
+    Mini.RGB1.setRGB(0,255,0);
+  }else{
+    //代表奇數
+    Mini.RGB1.setRGB(0,0,255);
+  }
   
-  delay(1000);
+  delay(2000);
+  Mini.RGB1.setRGB(0,0,0);
+  delay(2000);
 }
