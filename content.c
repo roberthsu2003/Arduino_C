5dc6aabf31da708d9de743b2af63da694a29051a
Robert Hsu <roberthsu2003@gmail.com>
2021-07-31 20:00:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,11 @@
-#include <MatrixMini.h>
-
 void setup() {
-  Mini.begin();
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("小車按鈕和RGB LED測試");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Mini.BTN1.get()){
-    Mini.RGB1.setRGB(255,0,0);
-  }else{
-    Mini.RGB1.setRGB(0,0,0);
-  }
-
-  if(Mini.BTN2.get()){
-    Mini.RGB2.setRGB(0,255,0);
-  }else{
-    Mini.RGB2.setRGB(0,0,0);
-  }
+  Serial.println(analogRead(A1));
   delay(50);
 }
