c298a572c3ddb39811fc252a57e68c38b6844ee8
Robert Hsu <roberthsu2003@gmail.com>
2021-08-07 19:42:12 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,9 @@
+#include <MatrixMini.h>
 #define LIGHT_SENSOR A1
 #define LIGHT_DIGITAL A0
 
 void setup() {
+  Mini.begin();
   Serial.begin(9600);
   Serial.println("============光每電阻測試=============");
   pinMode(LIGHT_DIGITAL,INPUT);
@@ -15,5 +17,12 @@ void loop() {
   Serial.println("==========光敏電阻類比訊號輸出==============");
   int analogValue=analogRead(LIGHT_SENSOR);
   Serial.println("類比訊號是:" + String(analogValue));
+  if(digitalValue == 0){
+    Mini.RGB1.setRGB(0,0,0);
+    Mini.RGB2.setRGB(0,0,0);
+  }else{
+    Mini.RGB1.setRGB(255,0,0);
+    Mini.RGB2.setRGB(255,0,0);
+  }
   delay(1000);
 }
