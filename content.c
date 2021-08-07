638a9706889841399ea9d46e8ff25ebfec45876d
Robert Hsu <roberthsu2003@gmail.com>
2021-08-07 19:33:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,23 +1,19 @@
-//每隔2秒，檢查如果是偶數，亮綠燈，如果是奇數就亮紅燈
-#include <MatrixMini.h>
+#define LIGHT_SENSOR A1
+#define LIGHT_DIGITAL A0
 
 void setup() {
-  Mini.begin();
   Serial.begin(9600);
-  randomSeed(analogRead(A1));
+  Serial.println("============光每電阻測試=============");
+  pinMode(LIGHT_DIGITAL,INPUT);
 }
 
 void loop() {
-  int randomNum = random(1001);
-  if(randomNum % 2 == 0){
-    //代表偶數
-    Mini.RGB1.setRGB(0,255,0);
-  }else{
-    //代表奇數
-    Mini.RGB1.setRGB(0,0,255);
-  }
-  
-  delay(2000);
-  Mini.RGB1.setRGB(0,0,0);
-  delay(2000);
+  Serial.println("==========光敏電阻數位訊號輸出==============");
+  bool digitalValue=digitalRead(LIGHT_DIGITAL);
+  Serial.println("數位訊號是:" + String(digitalValue));
+  Serial.println("\n\n");
+  Serial.println("==========光敏電阻類比訊號輸出==============");
+  int analogValue=analogRead(LIGHT_SENSOR);
+  Serial.println("類比訊號是:" + String(analogValue));
+  delay(1000);
 }
