18d58a595aa6facd646112d524d0afe1e98883bd
Robert Hsu <roberthsu2003@gmail.com>
2021-08-14 20:12:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,5 @@
-//藍線A0 - Trig
-//白線A1 - Echo
-
-
-#include "NewPing.h"
-#define TRIGGER_PIN A0
-#define ECHO_PIN A1
-#define MAX_DISTANCE 200
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+#define analogSensor A2
+#define digitalSensor A3
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,9 +7,13 @@ void setup() {
 }
 
 void loop() {
-  delay(50);
-  int uS = sonar.ping();
-  Serial.print("Ping:");
-  Serial.print(uS / US_ROUNDTRIP_CM);
-  Serial.println("cm");
+  int analogValue = analogRead(analogSensor);
+  int digitalValue = digitalRead(digitalSensor);
+
+  Serial.println("暗亮訊號值");
+  Serial.print("類比訊號:");
+  Serial.println(analogValue);
+  Serial.print("數位訊號:");
+  Serial.println(digitalValue);
+  delay(500);
 }
