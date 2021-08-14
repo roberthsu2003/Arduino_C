992fcb6a9ee6837b77562a9cbf94081dff315cf8
Robert Hsu <roberthsu2003@gmail.com>
2021-08-14 19:48:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,17 +1,23 @@
 //藍線A0 - Trig
 //白線A1 - Echo
 
+
 #include "NewPing.h"
 #define TRIGGER_PIN A0
 #define ECHO_PIN A1
 #define MAX_DISTANCE 200
 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(50);
+  int uS = sonar.ping();
+  Serial.print("Ping:");
+  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.println("cm");
 }
