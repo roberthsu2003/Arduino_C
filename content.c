bf948e873e8fd7a4a86b4cea0235ad6cb9688010
Robert Hsu <roberthsu2003@gmail.com>
2021-08-21 20:23:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,14 +2,20 @@
 
 void setup() {
   Mini.begin();
-  Mini.M1.set(-50);
-  Mini.M2.set(50);
-  delay(5000);
-  Mini.M1.set(0);
-  Mini.M2.set(0);
+  turnCar(50);
+  delay(3000);
+  turnCar(-50);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void turnCar(int i){
+   Mini.M1.set(-i);
+   Mini.M2.set(i);
+   delay(5000);
+   Mini.M1.set(0);
+   Mini.M2.set(0);
 }
