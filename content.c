5de1380b33dde6eefa68f6ee5a64e01af48cb37f
Robert Hsu <roberthsu2003@gmail.com>
2021-08-21 20:05:05 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,20 +2,9 @@
 
 void setup() {
   Mini.begin();
-  int i = 30;
-  //前進
-  Mini.M1.set(i);
-  Mini.M2.set(i);
-  delay(2000);
-  //停
   Mini.M1.set(0);
-  Mini.M2.set(0);
-  delay(2000);
-  //後退
-  Mini.M1.set(-i);
-  Mini.M2.set(-i);
-  delay(2000);
-  //停
+  Mini.M2.set(50);
+  delay(10000);
   Mini.M1.set(0);
   Mini.M2.set(0);
 }
