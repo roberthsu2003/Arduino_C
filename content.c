a06d82fe889dc6ae5b10052221a36d3b0c8611d3
Robert Hsu <roberthsu2003@gmail.com>
2021-08-28 19:22:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,19 @@
-#include <MatrixMini.h>
+#define LIGHT_DIGITOR A0
+#define LIGHT_SENSOR A1
 
 void setup() {
-  Mini.begin();
-  turnCar(50);
-  delay(3000);
-  turnCar(-50);
+  Serial.begin(9600);
+  pinMode(LIGHT_DIGITOR, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
-
-void turnCar(int i){
-   Mini.M1.set(-i);
-   Mini.M2.set(i);
-   delay(5000);
-   Mini.M1.set(0);
-   Mini.M2.set(0);
+  Serial.println("=====光敏電阻數位訊號===========");
+  bool digitalValue = digitalRead(LIGHT_DIGITOR);
+  Serial.println("數位訊號是:"+String(digitalValue));
+  Serial.println("\n\n");
+  Serial.println("=====光敏電阻類比訊號===========");
+  int analogValue = analogRead(LIGHT_SENSOR);
+  Serial.println("類比訊號是:"+String(analogValue));
+  delay(1000);
 }
