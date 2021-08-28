fc131ce0d62fefe2356f4e5271e9dd38fcbc5597
Robert Hsu <roberthsu2003@gmail.com>
2021-08-28 20:29:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,36 +1,20 @@
-#include <MatrixMini.h>
-#define LIGHT_DIGITOR A0
+#include <NewPing.h>
+#define TRIGGER_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 200
 
-bool turnOnce = false;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
 void setup() {
   Serial.begin(9600);
-  pinMode(LIGHT_DIGITOR, INPUT);
-  Mini.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("=====光敏電阻數位訊號===========");
-  bool digitalValue = digitalRead(LIGHT_DIGITOR);
-  Serial.println("數位訊號是:"+String(digitalValue));  
-  if (digitalValue == 1){
-    Serial.println("向右旋轉");
-    turn(30);
-    turnOnce = true;
-  }else{    
-    if(turnOnce == true){
-      Serial.println("向左旋轉");
-      turn(-30);
-    }
-    turnOnce = false;
-  }
-  delay(300);
-}
-
-void turn(int s){
-  Mini.M1.set(s);
-  Mini.M2.set(-s);
-  delay(3000);
-  Mini.M1.set(0);
-  Mini.M2.set(0);
+  int uS = sonar.ping();
+  int distance = uS / US_ROUNDTRIP_CM;
+  Serial.print("前放物件的距離:");
+  Serial.println(String(distance) + "公分");
+  delay(500);
 }
