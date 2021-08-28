b4204f39935fd580ed5ddc5cdbe2a2784b0182fb
Robert Hsu <roberthsu2003@gmail.com>
2021-08-28 19:54:07 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,10 @@
+#include <MatrixMini.h>
 #define LIGHT_DIGITOR A0
-
 
 void setup() {
   Serial.begin(9600);
   pinMode(LIGHT_DIGITOR, INPUT);
+  Mini.begin();
 }
 
 void loop() {
@@ -12,7 +13,16 @@ void loop() {
   bool digitalValue = digitalRead(LIGHT_DIGITOR);
   Serial.println("數位訊號是:"+String(digitalValue));  
   if (digitalValue == 1){
-    Serial.println("旋轉");
+    Serial.println("向右旋轉");
+    turn(30);
   }
   delay(300);
+}
+
+void turn(int s){
+  Mini.M1.set(s);
+  Mini.M2.set(-s);
+  delay(3000);
+  Mini.M1.set(0);
+  Mini.M2.set(0);
 }
