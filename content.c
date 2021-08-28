1d46757c6844907f0e9a194237695c1da5bbc153
Robert Hsu <roberthsu2003@gmail.com>
2021-08-28 19:27:40 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,5 @@
 #define LIGHT_DIGITOR A0
-#define LIGHT_SENSOR A1
+
 
 void setup() {
   Serial.begin(9600);
@@ -10,10 +10,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("=====光敏電阻數位訊號===========");
   bool digitalValue = digitalRead(LIGHT_DIGITOR);
-  Serial.println("數位訊號是:"+String(digitalValue));
-  Serial.println("\n\n");
-  Serial.println("=====光敏電阻類比訊號===========");
-  int analogValue = analogRead(LIGHT_SENSOR);
-  Serial.println("類比訊號是:"+String(analogValue));
+  Serial.println("數位訊號是:"+String(digitalValue));  
   delay(1000);
 }
