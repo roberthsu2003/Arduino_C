f6a39c90fc953f8d0c079f5cd0e93e271c96d59d
Robert Hsu <roberthsu2003@gmail.com>
2021-09-04 20:30:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,26 +1,22 @@
-#include <NewPing.h>
-#define TRIGGER_PIN A0
-#define ECHO_PIN A1
-#define MAX_DISTANCE 200
+#include <MatrixMini.h>
 
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
-  Serial.begin(9600);
+  Mini.begin();  // create with the default frequency 1.6KHz
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  
+  Serial.println("\n Matrix Mini DC Motor Test \n");
+  int i=70;
+  
+  Mini.M1.set(i);  
+  Mini.M2.set(i);
+  delay(3000);
+  Mini.M1.set(0);
+  Mini.M2.set(0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int uS = sonar.ping();
-  int distance = uS / US_ROUNDTRIP_CM;
-  //多向選擇
-  if(distance <= 10 && distance != 0){
-    Serial.println("障礙物很接近");
-  }else if(distance <= 20 && distance != 0){
-    Serial.println("障礙物在11~20分分之間");
-  }else{
-    Serial.println("障礙物21公分以上");
-  }
-  delay(300);
+
+void loop() { 
+  
 }
