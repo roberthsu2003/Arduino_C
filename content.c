e2a3318833c116809fd9501ba154e10d2cdd04e3
Robert Hsu <roberthsu2003@gmail.com>
2021-10-09 19:28:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,5 +2,12 @@
 
 using namespace std;
 int main() {
-  cout << "Hello World!\n";
+	int scores[3];
+	scores[0] = 70;
+	scores[1] = 80;
+	scores[2] = 90;
+
+	cout << scores[0] << "," << scores[1] << "," << scores[2] << endl;
+
+	return 0;
 }
