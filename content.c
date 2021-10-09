92ea7f16a4aa2db475743b1e52ef0489cdf7ae8e
Robert Hsu <roberthsu2003@gmail.com>
2021-10-09 20:24:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,25 +1,12 @@
 #include <iostream>
+#include <time.h>
+
 using namespace std;
-
 int main() {
-	int nums;
-	int min = 99999;
-	int max = 0;
-	cout << "請輸入數值的數量:";
-	cin >> nums;
-	int values[nums];
-
-	for(int i=0; i<nums; i++){
-		cout << "請輸入第" << i+1 << "數值:";
-		cin >> values[i];
-	}
-
-	for(int i=0; i<nums; i++){
-		cout << "第" << i+1 << "個數值是" << values[i] << endl;
-		min = min < values[i] ? min : values[i];
-		max = max > values[i] ? max : values[i];
-	}
-
-	cout << "最大值是:" << max << ",最小值:" << min << endl;
-	return 0;	
+	srand(time(NULL));	
+	int max = 200;
+	int min = 100;
+	int randValue = rand() % (max-min+1) + min;
+	cout << "100~200之間的亂數:" << randValue << endl;
+	return 0;
 }
