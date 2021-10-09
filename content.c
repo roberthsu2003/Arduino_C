6fb8211034083dadce4986ad7330fd0fc84f2448
Robert Hsu <roberthsu2003@gmail.com>
2021-10-09 19:22:06 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,38 +1,6 @@
-#include <MatrixMini.h>
+#include <iostream>
 
-void setup() {
-  // put your setup code here, to run once:
-  Mini.begin();  // create with the default frequency 1.6KHz
-  Serial.begin(9600);
-}
-
-void loop() {
-  fireButton();
-  stopButton();
-}
-
-void fireButton(){
-   static bool previousState = false;
-   static int counter = 0;
-   bool currentState = Mini.BTN1.get();
-  if(currentState != previousState){    
-    counter += 1;
-    if(counter % 2 == 0){
-      Serial.println("防盜器啟動");      
-    }
-    previousState = currentState;
-  }
-}
-
-void stopButton(){
-  static bool previousState = false;
-  static int counter = 0;
-  bool currentState = Mini.BTN2.get();
-  if(currentState != previousState){    
-    counter += 1;    
-    if(counter % 2 == 0){
-      Serial.println("防盜器關閉");     
-    }
-    previousState = currentState;
-  }
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
 }
