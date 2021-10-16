cbfa9325f5e040856cb9ea12bea34febae32ded2
Robert Hsu <roberthsu2003@gmail.com>
2021-10-16 20:17:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,19 +1,38 @@
-int nums = 100;
+#include <MatrixMini.h>
 void setup() {
+  Mini.begin();
   Serial.begin(9600);
-  int array[nums];
-  
-  for(int i=0; i<nums; i++){
-    array[i] = random(10,51);
-  }
-
-  for(int i=0; i<nums; i++){
-    Serial.println(array[i]);
-  }
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  leftButton();
+  rightButton();
 }
+
+void leftButton(){
+  static bool previousState = false;
+   static int counter = 0;
+   bool currentState = Mini.BTN1.get();
+  if(currentState != previousState){    
+    counter += 1;
+    if(counter % 2 == 0){
+      Serial.println("左按鈕被按了一下");     
+    }
+    previousState = currentState;
+  }
+}
+
+void rightButton(){
+  static bool previousState = false;
+   static int counter = 0;
+   bool currentState = Mini.BTN2.get();
+  if(currentState != previousState){    
+    counter += 1;
+    if(counter % 2 == 0){
+      Serial.println("右按鈕被按了一下");     
+    }
+    previousState = currentState;
+  }
+}
+
