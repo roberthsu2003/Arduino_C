315943ef6ddd15daa76512fd57babebe580b1dd7
Robert Hsu <roberthsu2003@gmail.com>
2021-10-16 19:50:01 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,40 +1,19 @@
-#include <iostream>
-using namespace std;
+int nums = 100;
+void setup() {
+  Serial.begin(9600);
+  int array[nums];
+  
+  for(int i=0; i<nums; i++){
+    array[i] = random(10,51);
+  }
 
-int main() {
-	int nums;
-	cout << "請輸入要排序的數值個數:";
-	cin >> nums;
-	//建立陣列
-	int array[nums];
-	//給陣列內容
-	for(int i=0; i<nums; i++){
-		cout << "請輸入第" << i+1 << "個數值:";
-		cin >> array[i];
-	}
-	//排序前的內容輸出
-	cout << "排序前:" << endl;
-	for(int i=0; i<nums; i++){
-		cout << array[i] << " ";
-	}
+  for(int i=0; i<nums; i++){
+    Serial.println(array[i]);
+  }
+  
+}
 
-	//陣列排序
-	for(int i=0;i<nums-1;i++){
-		for(int j=i+1;j<nums;j++){
-			if(array[i] > array[j]){
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-	}
-	cout << endl;
+void loop() {
+  // put your main code here, to run repeatedly:
 
-	//排序前的內容輸出
-	cout << "排序後:" << endl;
-	for(int i=0; i<nums; i++){
-		cout << array[i] << " ";
-	}
-
-	cout << endl;
 }
