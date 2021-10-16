1f6935f9b72767f434925c21d19e051a10aeea3a
Robert Hsu <roberthsu2003@gmail.com>
2021-10-16 19:36:42 +0800

    Update content.c

--- content.c
+++ content.c
@@ -17,4 +17,24 @@ int main() {
 	for(int i=0; i<nums; i++){
 		cout << array[i] << " ";
 	}
+
+	//陣列排序
+	for(int i=0;i<nums-1;i++){
+		for(int j=i+1;j<nums;j++){
+			if(array[i] > array[j]){
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	cout << endl;
+
+	//排序前的內容輸出
+	cout << "排序後:" << endl;
+	for(int i=0; i<nums; i++){
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
 }
