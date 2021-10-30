bd23cb61698dbc4318da37c616dc6c8a6fbaadd3
Robert Hsu <roberthsu2003@gmail.com>
2021-10-30 19:26:13 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,7 +4,22 @@ using namespace std;
 int main() {
 	int min = 1;
 	int max = 99;
+	int keyin;
+	int count = 0;
 	srandom(time(NULL));
 	int guess = random() % (max-min+1) + 1;
-	cout << guess << endl;
+	cout << "=================猜數字遊戲================\n\n";
+	while(true){
+		cout << "猜數字範圍" << min << "~" << max << ":";
+		cin >> keyin;
+		count += 1;
+		if(keyin >= min && keyin <= max){
+			cout << "範圍正確" << endl;
+			break;
+		}else{
+			cout << "請輸入提示範圍內的數字" << endl;
+		}
+	}
+
+	cout << "遊戲結束" << endl;
 } 
