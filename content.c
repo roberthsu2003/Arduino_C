f47d4da6a78b0c82ec0de1ec5789de9ff88f7639
徐國堂 <roberthsu2003@gmail.com>
2022-01-15 11:08:33 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,9 +4,21 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   int englishScore = 60;
-  Serial.println("目前英文分數是");
+  Serial.print("目前英文分數是");
   Serial.println(englishScore);
   Serial.println("請輸入您的英文分數:");
+  while(true){
+    //等待使用者輸入
+    char letters[2];
+    if(Serial.available() > 0){      
+        englishScore = Serial.parseInt();
+        break;
+    }
+    
+  }
+
+  Serial.print("新的英文分數是:");
+  Serial.println(englishScore);
 
 }
 
