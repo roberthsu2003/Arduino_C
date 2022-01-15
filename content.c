0ec536d7318564b4c779ef36ced50fab1eb14582
徐國堂 <roberthsu2003@gmail.com>
2022-01-15 11:29:34 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,6 @@
-#include <arduino.h>
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  int englishScore = 60;
-  Serial.print("目前英文分數是");
-  Serial.println(englishScore);
-  Serial.println("請輸入您的英文分數:");
-  while(true){
-    //等待使用者輸入
-    char letters[2];
-    if(Serial.available() > 0){      
-        englishScore = Serial.parseInt();
-        break;
-    }
-    
-  }
-
-  Serial.print("新的英文分數是:");
-  Serial.println(englishScore);
+  Serial.println("請輸入13pinLed,閃爍的次數:")
 
 }
 
