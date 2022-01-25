8560b5bb4d58796a7d4209aa9d9b0181cdac7885
徐國堂 <roberthsu2003@gmail.com>
2022-01-25 16:49:31 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,20 +1,14 @@
-#define R 3
-#define G 5
-#define B 6
-
-String stringBuffer;
-
-void setup() {
-  Serial.begin(9600);
-
-}
-
-void loop() {
+/*--------------------------------
+ *使用者,可以在arudino編輯器的序列埠管理員
+ *輸入3個數值,格式如下: 255,255,0
+ *rgb參數必需傳入3個int元素的陣列
+ *
+----------------------------- */
+void inputMultipleValue(int* rgb){
   if(Serial.available() > 0){
-    stringBuffer = Serial.readString();    
+    String stringBuffer = Serial.readString();    
     int stringLength = stringBuffer.length();
-    String rString = ""; 
-    int rgbArray[3];
+    String rString = "";     
     int j = 0;
      
     for(int i=0; i<stringLength; i++){
@@ -22,13 +16,13 @@ void loop() {
       if(stringBuffer[i] != ',' ){ 
         rString += stringBuffer[i];       
       }else{
-        rgbArray[j] = rString.toInt();
+        rgb[j] = rString.toInt();
         j++;
         rString = "";
       }
 
       if(i == stringLength-1){
-        rgbArray[j] = rString.toInt();
+        rgb[j] = rString.toInt();
         j++;
          rString = "";
       } 
@@ -36,10 +30,35 @@ void loop() {
       
     }
 
-    Serial.println(rgbArray[0]);
-    Serial.println(rgbArray[1]);
-    Serial.println(rgbArray[2]);
+    
     
   }
+}
+
+
+
+
+
+
+
+
+#include "inputThreeValue.h"
+int rgb[3] = {0, 0, 0};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  inputMultipleValue(rgb);
+  Serial.print('R=');
+  Serial.println(rgb[0]);
+  Serial.print('G=');
+  Serial.println(rgb[1]);
+  Serial.print('B=');
+  Serial.println(rgb[2]);
+  Serial.println("========================");
+  delay(3000);
 
 }
