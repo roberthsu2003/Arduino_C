b5ec5d6a14cae4718b6c760618af343398c8f679
徐國堂 <roberthsu2003@gmail.com>
2022-01-28 14:23:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,23 @@
 #include <IRremote.h>
+#define IRPin 7
+
+IRrecv irrecv(IRPin);
+decode_results results;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  irrecv.enableIRIn();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(irrecv.decode(&results)){
+    Serial.print("irCode:");
+    Serial.print(results.value, Hex);
+    Serial.print(",bits:");
+    Serial.println(results.bits);
+    irrecv.resume();
+  }
 
+  delay(600);
 }
